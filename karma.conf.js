c86ac6027bb413428e9cdb4d0f2ed06a5a7015cd
Nikhita Anilkumar <nikhitaanilkumar6@gmail.com>
2023-03-21 23:27:08 -0400

    updates package.json file

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,6 @@
 module.exports = function(config) {
     config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client: {
-        clearContext: false // leave Jasmine Spec Runner output visible in browser
-      },
-      jasmineHtmlReporter: {
-        suppressAll: true // removes the duplicated traces
-      },
-      coverageReporter: {
-        dir: require('path').join(__dirname, '../coverage'),
-        reports: ['html', 'lcovonly'],
-        fixWebpackSourcePaths: true
-      },
-      reporters: ['progress', 'kjhtml'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: true,
+
       browsers: ['ChromeHeadlessNoSandbox'],
       customLaunchers: {
         ChromeHeadlessNoSandbox: {
@@ -36,8 +12,7 @@ module.exports = function(config) {
             ]
         }
       },
-      singleRun: false,
-      restartOnFileChange: true
+      singleRun: true      
     });
   };
   
